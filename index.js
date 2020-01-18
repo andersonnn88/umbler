f91e6053f8ab0c6ee0a6290cab3c7f2a184c204c
Anderson Araujo <andersonga4@gmail.com>
2020-01-18 16:27:38 -0300

    versao 2

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (req,res) =>{
 
 })
 
-app.listen(3000);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Umbler listening on port %s', port);
+});
